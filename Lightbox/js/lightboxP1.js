29c8ead0337134689d0279a5ff7ffe9835eba474
GDWD-KC <casey.tobias@first.edu>
2022-12-06 14:05:13 -0500

    wk26day3

---
+++ Lightbox/js/lightboxP1.js
@@ -0,0 +1,33 @@
+//Variables for lightbox elements
+let lightboxContainer = document.getElementById('lightboxContainer');
+let lightboxBackground = document.getElementById('lightboxBackground');
+let lightbox = document.getElementById('lightbox');
+let lightboxCloser = document.getElementById('lightboxCloser');
+
+//Function to open lightbox
+function openLightbox(){
+    //Turn on display class (from display:none to display: flex)
+    lightboxContainer.classList.add('display');
+    //Display image src inside lightbox
+    lightbox.src = this.src;
+}
+
+let coffeeImage = document.getElementById('coffeeImage1');
+//When you click image, reference openLightBox function
+coffeeImage.onclick = openLightbox;
+
+//Make Function to make closer clickable
+function closeLightbox(){
+    lightboxContainer.classList.remove('display');
+    lightbox.src = "";
+}
+
+lightboxCloser.onclick = closeLightbox;
+lightboxBackground.onclick = closeLightbox;
+
+
+
+
+
+
+
--- Practice/js/practice.js
+++ Practice/js/practice.js
@@ -1,23 +1,11 @@
-let squareWidth = 100;
-let squareHeight = 100;
-
-let color1 = document.getElementById("square1");
-let color2 = document.getElementById("square2");
-let color3 = document.getElementById("square3");
-
-
-color1.style.height = squareHeight + "px";
-color1.style.width = squareWidth + "px";
-color1.style.backgroundColor = "lightblue";
-
-color2.style.height = squareHeight + "px";
-color2.style.width = squareWidth + "px";
-color2.style.backgroundColor = "lightgreen";
-
-color3.style.height = squareHeight + "px";
-color3.style.width = squareWidth + "px";
-color3.style.backgroundColor = "plum";
-
-function bgChange(bg) {
-    document.body.style.background = bg;
+//Choose the larger number
+function chooseLargerNumber(num1, num2) {
+    let larger;
+    if(num1 >= num2) {
+        larger = num1;
+    } else {
+        larger = num2;
+    }
+    return larger;
 }
+
