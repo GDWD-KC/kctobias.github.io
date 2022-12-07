ece53f71f9d5fc30639c417db7cb9999c819b81a
KC-Louise <110142726+GDWD-KC@users.noreply.github.com>
2022-12-07 18:10:19 -0500

    y

--- Practice/js/practice.js
+++ Practice/js/practice.js
@@ -1,11 +1,1 @@
-//Choose the larger number
-function chooseLargerNumber(num1, num2) {
-    let larger;
-    if(num1 >= num2) {
-        larger = num1;
-    } else {
-        larger = num2;
-    }
-    return larger;
-}
 
