016ac86f623404e1beafa1075a054d1b03e8eeeb
GDWD-KC <casey.tobias@first.edu>
2022-12-14 14:44:35 -0500

    wk27day4

---
+++ FinalEval/disney.js
@@ -0,0 +1,9 @@
+//Set variables to utilize content
+const shows = document.getElementById("shows");
+const tabs = document.getElementsByClassName(".tabs");
+const content = document.getElementsByClassName(".content");
+
+//Add on click event listener to cycle through content by tab click
+
+
+
