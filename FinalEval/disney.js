70e4fa212f05064981e6a8c3403f0b07a5b220c1
KC-Louise <110142726+GDWD-KC@users.noreply.github.com>
2022-12-14 18:52:52 -0500

    t

--- FinalEval/disney.js
+++ FinalEval/disney.js
@@ -1,8 +1,29 @@
 //Set variables to utilize content
 const shows = document.getElementById("shows");
-const tabs = document.getElementsByClassName(".tab-btns");
-const content = document.getElementsByClassName(".content");
+const tabs = document.querySelectorAll(".tab-btn");
+const content = document.querySelectorAll(".content");
+
+//Get variables for each tab
 
 //Add on click event listener to cycle through content by tab click
 
+//Refactored Way
 
+shows.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    tabs.forEach(function (btn) {
+      btn.classList.remove("active");
+      btn.classList.remove("highlight");
+    });
+    e.target.classList.add("active");
+    e.target.classList.add("highlight");
+    // hide other articles
+    content.forEach(function (images) {
+      images.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
