4adf40752435b8815dc2c26e44b4c0ea1cfdd7d2
KC-Louise <110142726+GDWD-KC@users.noreply.github.com>
2022-12-14 19:32:41 -0500

    h

--- FinalEval/disney.js
+++ FinalEval/disney.js
@@ -21,15 +21,3 @@ shows.addEventListener("click", function (e) {
     element.classList.add("active");
   }
 });
-//On scroll nav event
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-50px";
-//   }
-// }
